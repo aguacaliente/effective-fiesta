f14503d01cd6eb5e1a943419b787106959945b1a
aguacaliente <98557628+aguacaliente@users.noreply.github.com>
2023-04-09 12:53:29 +0100

    first commit flask framework

---
+++ server/app.py
@@ -0,0 +1,20 @@
+
+
+
+from flask import Flask
+from models import db, User
+from config import ApplicationConfig
+
+app = Flask(__name__)
+
+app.config.from_object(ApplicationConfig)
+
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
